27c5f4f3c9e4f363b84b364c43554be69a1c1b21
santhosh r ch <santhu3064@gmail.com>
2021-01-15 13:55:36 +0000

    Create string_breaker.py

---
+++ codewars/string_breaker.py
@@ -0,0 +1,29 @@
+
+# I will give you an integer (N) and a string. Break the string up into as many substrings of N as you can without spaces. If there are leftover characters, include those as well.
+# 
+# Example: 
+# 
+# n = 5;
+# 
+# st = "This is an example string";
+# 
+# Return value:
+# Thisi
+# sanex
+# ample
+# strin
+# g
+# 
+# Return value as a string: 'Thisi'+'\n'+'sanex'+'\n'+'ample'+'\n'+'strin'+'\n'+'g'
+
+def string_breakers(n, st): 
+    # your code here
+    finalstring= st.replace(" ", "")
+    l =len(st)
+    i =0 
+    a= ""
+    while i<=l:
+        a += finalstring[i:i+n] + "\n"
+        i += n
+    return a.rstrip("\n")
+        
